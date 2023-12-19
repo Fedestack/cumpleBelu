f7d54eec010e49bdea576120d5d262438c151ac3
Fedestack <124637861+Fedestack@users.noreply.github.com>
2023-12-19 20:46:20 -0300

    invitacion cumple belu

---
+++ script.js
@@ -0,0 +1,8 @@
+function confirmarAsistencia(respuesta) {
+    if (respuesta === 'Sí') {
+      alert('¡Genial! Esperamos verte en la fiesta. ¡Será una celebración increíble!');
+    } else {
+      alert('¡Lamentamos que no puedas asistir! Esperamos poder celebrar contigo en otra ocasión.');
+    }
+  }
+  
