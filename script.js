00d16f67018954bb008ac81356106a1f04eea9e6
FedeStack <fedenegreira@gmail.com>
2023-12-20 00:05:22 -0300

    modifi

--- script.js
+++ script.js
@@ -31,6 +31,28 @@ function confirmarAsistencia(respuesta) {
 
   // Simula un clic en el enlace.
   botonEnlace.click();
-
-  // Puedes agregar aquí cualquier otro código que necesites ejecutar después de enviar el mensaje.
 }
+
+function createConfeti() {
+  const confetiContainer = document.getElementById('confeti-container');
+
+  for (let i = 0; i < 100; i++) {
+    const confeti = document.createElement('div');
+    confeti.className = 'confeti';
+    confeti.style.left = `${Math.random() * 100}vw`;
+    confeti.style.animationDuration = `${Math.random() * 2 + 1}s`;
+    confeti.style.backgroundColor = getRandomColor();
+    confetiContainer.appendChild(confeti);
+  }
+}
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+// Llama a la función createConfeti cuando desees activar el efecto de confeti
+// Por ejemplo, puedes llamarla cuando se carga la página o cuando un usuario confirma su asistencia.
+window.addEventListener('load', createConfeti);
