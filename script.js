373380e5816fa5075f50f931d500ffa80234fa0d
FedeStack <fedenegreira@gmail.com>
2023-12-19 21:16:53 -0300

    creando links botones a whatsapp

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function confirmarAsistencia(respuesta) {
-    if (respuesta === 'Sí') {
-      alert('¡Genial! Esperamos verte en la fiesta. ¡Será una celebración increíble!');
-    } else {
-      alert('¡Lamentamos que no puedas asistir! Esperamos poder celebrar contigo en otra ocasión.');
-    }
+  let mensaje = '';
+
+  if (respuesta === 'Sí') {
+    mensaje = 'Confirmo mi asistencia a la fiesta. Mi nombre es:';
+  } else {
+    mensaje = 'No podré asistir a la fiesta. Mi nombre es:';
   }
-  
+
+  // Reemplaza "TUNUMERODETELEFONO" con tu número de teléfono, incluyendo el código de país sin el símbolo "+".
+  let telefono = 'TUNUMERODETELEFONO';
+
+  // Abre la interfaz de WhatsApp con el número y el mensaje predefinido.
+  window.open(`https://api.whatsapp.com/send?phone=${telefono}&amp;text=${encodeURIComponent(mensaje)}`, '_blank');
+
+  // Puedes agregar aquí cualquier otro código que necesites ejecutar después de enviar el mensaje.
+}
