f6829b069c67d507547b63ec94d082f8e7d6cf7f
FedeStack <fedenegreira@gmail.com>
2023-12-19 21:39:28 -0300

    modificaciones

--- script.js
+++ script.js
@@ -2,16 +2,31 @@ function confirmarAsistencia(respuesta) {
   let mensaje = '';
 
   if (respuesta === 'Sí') {
-    mensaje = 'Confirmo mi asistencia a la fiesta. Mi nombre es:';
+    mensaje = 'Confirmo que asistiré a la fiesta de MARÍA BELÉN. Mi nombre y apellido es:';
   } else {
-    mensaje = 'No podré asistir a la fiesta. Mi nombre es:';
+    mensaje = 'No podré asistir a la fiesta. Mi nombre y apellido es:';
   }
 
   // Reemplaza "TUNUMERODETELEFONO" con tu número de teléfono, incluyendo el código de país sin el símbolo "+".
-  let telefono = 'TUNUMERODETELEFONO';
+  let telefono = '541158045379';
 
-  // Abre la interfaz de WhatsApp con el número y el mensaje predefinido.
-  window.open(`https://api.whatsapp.com/send?phone=${telefono}&amp;text=${encodeURIComponent(mensaje)}`, '_blank');
+  // Crea un enlace de WhatsApp con el número y el mensaje predefinido.
+  let enlaceWhatsApp = document.createElement('a');
+  enlaceWhatsApp.href = `https://api.whatsapp.com/send?phone=${telefono}&text=${encodeURIComponent(mensaje)}`;
+  enlaceWhatsApp.target = '_blank';
+
+  // Crea un botón dentro del enlace (puedes personalizar esto según tus necesidades).
+  let botonEnlace = document.createElement('button');
+   botonEnlace.textContent = 'Ir a WhatsApp';
+
+  // Agrega el enlace al botón.
+  enlaceWhatsApp.appendChild(botonEnlace);
+
+  // Agrega el enlace al DOM (fuera del flujo normal del documento para que no afecte el diseño).
+  document.body.appendChild(enlaceWhatsApp);
+
+  // Simula un clic en el enlace.
+  botonEnlace.click();
 
   // Puedes agregar aquí cualquier otro código que necesites ejecutar después de enviar el mensaje.
 }
